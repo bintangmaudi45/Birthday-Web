51abd1013e4dc959feb19aa011b92670ac00ef26
bintangmaudi45 <bintangmaudi45@gmail.com>
2026-08-25 02:46:26 +0700

    fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Birthday-Web/',
   plugins: [react()],
   resolve: {
     alias: {
